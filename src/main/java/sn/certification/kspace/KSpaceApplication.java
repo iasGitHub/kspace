947b48a1e8925e54a8a8957c122ee6adb273b27c
idriss <idrisssall@yahoo.fr>
2022-06-15 17:52:18 +0000

    création du projet et configuration du fichier app.properties

---
+++ src/main/java/sn/certification/kspace/KSpaceApplication.java
@@ -0,0 +1,13 @@
+package sn.certification.kspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/certification/kspace/KSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.certification.kspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
